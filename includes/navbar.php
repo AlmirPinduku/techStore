a8de351a921a5223988c8221f0c58668d8007970
Almir <almirpinduku@gmail.com>
2022-10-03 10:59:56 +0200

    shoppingcardicon

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,6 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="styles.css" rel="stylesheet">
 	<script src="https://cdn.tailwindcss.com"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 <body>
 	<header class="text-gray-400 bg-black body-font w-full">
@@ -18,6 +20,7 @@
 				<a class="mr-5 text-white text-lg" href="includes/katalogu.php">Produkts</a>
 				<a class="mr-5 text-white text-lg" href="includes/rreth_nesh.php">About Us</a>
 			</nav>
+			<a class="mr-7 text-white text-lg" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
 			<a class="mr-5 text-white text-lg" href="admin/login.php">Login</a>
 		</div>
 	</header>
