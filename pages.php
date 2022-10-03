4dcc99b8a1825b95e1005b56134c1e738e2469eb
Almir <almirpinduku@gmail.com>
2022-10-03 12:56:36 +0200

    somechange_db/page_update

--- pages.php
+++ pages.php
@@ -3,6 +3,7 @@
 		<title>Miresevini ne marketin online</title>
 		        <link href="styles.css" rel="stylesheet">
             <link href="./input.css" rel="stylesheet">
+            <script src="https://cdn.tailwindcss.com"></script>
 	</head>
 
 <body>
@@ -35,7 +36,7 @@ while($row=mysqli_fetch_array($run_query)){
   <div class="container px-5 py-60 mx-auto">
     <div class="lg:w-4/5 mx-auto flex flex-wrap">
 	<img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="images/<?php echo $produkt_image; ?>"/>      <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">Apple</h2>
+        <h2 class="text-sm title-font text-gray-500 tracking-widest"><?php echo $produkt_keywords; ?></h2>
         <h1 class="text-white text-3xl title-font font-medium mb-1"><?php echo $produkt_title; ?></h1>
         <div class="flex mb-4">
           <span class="flex items-center">
@@ -76,26 +77,10 @@ while($row=mysqli_fetch_array($run_query)){
         </div>
         <p class="leading-relaxed"><?php echo $produkt_content; ?></p>
         <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-500 mb-5">
-          <div class="flex">
-            <span class="mr-3">Color</span>
-            <button class="border-2 border-gray-800 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button class="border-2 border-gray-800 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button class="border-2 border-gray-800 ml-1 bg-yellow-500 rounded-full w-6 h-6 focus:outline-none"></button>
+          <div class="flex"> 
           </div>
           <div class="flex ml-6 items-center">
-            <span class="mr-3">Size</span>
             <div class="relative">
-              <select class="rounded border border-gray-300 focus:ring-2 focus:ring-gray-900 bg-transparent appearance-none py-2 focus:outline-none focus:border-gray-500 text-white pl-3 pr-10">
-                <option>SM</option>
-                <option>M</option>
-                <option>L</option>
-                <option>XL</option>
-              </select>
-              <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
-                  <path d="M6 9l6 6 6-6"></path>
-                </svg>
-              </span>
             </div>
           </div>
         </div>
